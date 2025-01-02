8f8a2bc2d32f0fd75f33ab583b1b1faacd60c0f7
Frank <franco.arteche.2b@gmail.com>
2025-01-02 16:26:42 -0300

    exercise 1.9

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,16 +1,23 @@
 import { useState } from 'react'
 
 const Statistics = ({good,neutral,bad}) => {
-  return (
+  if(good+neutral+bad > 0){
+    return (
+      <div>
+          <h1>statistics</h1>
+          <p>good: {good}</p>
+          <p>neutral: {neutral}</p>
+          <p>bad: {bad}</p>
+          <p>all: {good+neutral+bad}</p>
+          <p>average: {(good-bad)/(good+neutral+bad)}</p>
+          <p>positive: {good/(good+neutral+bad)*100} %</p>
+        </div>
+    )
+  } else return (
     <div>
-        <h1>statistics</h1>
-        <p>good: {good}</p>
-        <p>neutral: {neutral}</p>
-        <p>bad: {bad}</p>
-        <p>all: {good+neutral+bad}</p>
-        <p>average: {(good-bad)/(good+neutral+bad)}</p>
-        <p>positive: {good/(good+neutral+bad)*100} %</p>
-      </div>
+      <h1>statistics</h1>
+      <p>No feedback given</p>
+    </div>
   )
 }
 
