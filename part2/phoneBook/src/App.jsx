748f04a44bee0211d4bbfd2aebc54cf6f0048f60
Frank <franco.arteche.2b@gmail.com>
2025-01-05 19:05:43 -0300

    Exercise 2.8 done

--- part2/phoneBook/src/App.jsx
+++ part2/phoneBook/src/App.jsx
@@ -1,66 +1,82 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
-  const [newName, setNewName] = useState("");
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-1234567" },
+  ]);
+  const [newName, setNewName] = useState({ name: "", number: "" });
 
   const addPerson = (e) => {
     e.preventDefault();
     const personObject = {
-      name: newName,
+      name: newName.name,
+      number: newName.number,
     };
 
     let canAddPerson = persons.find((item) => {
-      return areTheseObjectsEqual(item,personObject)
-    })
+      return areTheseObjectsEqual(item, personObject);
+    });
 
-    if(canAddPerson){
-      alert(`${newName} is already added to phonebook`)
+    if (canAddPerson) {
+      alert(`${newName} is already added to phonebook`);
       return;
     }
 
     setPersons(persons.concat(personObject));
     setNewName("");
-  }
+  };
 
   function areTheseObjectsEqual(first, second) {
     const al = Object.getOwnPropertyNames(first);
     const bl = Object.getOwnPropertyNames(second);
-  
+
     // Check if the two list of keys are the same
     // length. If they are not, we know the objects
     // are not equal.
     if (al.length !== bl.length) return false;
-  
+
     // Check that all keys from both objects match
     // are present on both objects.
     const hasAllKeys = al.every((value) => !!bl.find((v) => v === value));
-  
+
     // If not all the keys match, we know the
     // objects are not equal.
     if (!hasAllKeys) return false;
-  
+
     // We can now check that the value of each
     // key matches its corresponding key in the
     // other object.
     for (const key of al) if (first[key] !== second[key]) return false;
-  
+
     // If the object hasn't return yet, at this
     // point we know that the objects are the
     // same
     return true;
   }
 
-  const handleInputChange = (e) => {
-    setNewName(e.target.value);
-  }
+  const handleInputChange = (e, input) => {
+    console.log(e, input);
+
+    setNewName({ ...newName, [input]: e.target.value });
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form  onSubmit={addPerson}>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handleInputChange} />
+          name:{" "}
+          <input
+            value={newName.name}
+            onChange={(e) => handleInputChange(e, "name")}
+          />
+        </div>
+        <div>
+          number:{" "}
+          <input
+            value={newName.number}
+            onChange={(e) => handleInputChange(e, "number")}
+          />
         </div>
         <div>
           <button type="submit">add</button>
@@ -68,7 +84,13 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {persons.map((person) => {
-        return <h4>{person.name}</h4>
+        return (
+          <>
+            <div>
+              <h4>{person.name} {person.number}</h4>
+            </div>
+          </>
+        );
       })}
     </div>
   );
