0aa63c534893d4b24a0fa9eeaa50ff9ca9bca3af
Frank <franco.arteche.2b@gmail.com>
2025-01-05 22:24:10 -0300

    Exercise 2.11 done

--- part2/phoneBook/src/App.jsx
+++ part2/phoneBook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const SearchField = ({ handleSearch, inputSearch }) => {
   return (
@@ -38,29 +39,26 @@ const Persons = ({ persons }) => {
       <h2>Numbers</h2>
       {persons.map((person) => {
         return (
-          <>
             <div key={person.id}>
               <h4>
                 {person.name} {person.number}
               </h4>
             </div>
-          </>
         );
       })}
     </>
   );
 };
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
-
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState({ name: "", number: "" });
-
   const [inputSearch, setInputSearch] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
 
   const addPerson = (e) => {
     e.preventDefault();
