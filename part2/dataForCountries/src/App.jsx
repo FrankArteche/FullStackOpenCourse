1c371b81533153704c9baf0b3bcdb5b0c2da055e
Frank <franco.arteche.2b@gmail.com>
2025-01-06 01:06:54 -0300

    Exercise 2.18 done
    - App now works properly, searching and showing data correctly

--- part2/dataForCountries/src/App.jsx
+++ part2/dataForCountries/src/App.jsx
@@ -1,28 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import axios from "axios"
+import { useState, useEffect } from "react";
+
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [countries, setCountries] = useState(0)  
-  const [search, setSearch] = useState('')
+  const [countries, setCountries] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
+      .then((response) => {
+        setCountries(response.data);
+      });
+  }, []);
 
   const handleSearch = (event) => {
-    setSearch(event.target.value)
+    setSearch(event.target.value);
 
-    axios.get(`https://studies.cs.helsinki.fi/restcountries/api/name/${event.target.value}`)
-  }
-  
+    let countriesSearched = countries.filter((country) =>
+      country.name.common.toLowerCase().includes(search.toLowerCase())
+    );
+    console.log(countriesSearched);
+    
+
+    setCountries(countriesSearched);
+  };
+
   return (
     <>
       <div>
-        find countries <input onChange={handleSearch} value={search}/>
-
+        find countries <input onChange={handleSearch} value={search} />
+        {countries.length > 10 && (
+          <p>Too many matches, specify another filter</p>
+        )}
+        {countries.length <= 10 &&
+          countries.length > 1 &&
+          countries.map((country) => (
+            <p key={country.name.common}>{country.name.common}</p>
+          ))}
+        {countries.length == 1 && (
+          <div>
+            <h1>{countries[0].name.common}</h1>
+            <p>capital: {countries[0].capital[0]}</p>
+            <p>area: {countries[0].area}</p>
+            <h2>Languages:</h2>
+            <ul>
+              {Object.values(countries[0].languages).map((language) => (
+                <li key={language}>{language}</li>
+              ))} 
+            </ul>
+            <img
+            src={countries[0].flags.png}></img>
+          </div>
+        )}
       </div>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
