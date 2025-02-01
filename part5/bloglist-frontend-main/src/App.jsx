415dfc6dc1033fb8cf8d5dab22a03f2f11fc5ead
Frank <franco.arteche.2b@gmail.com>
2025-02-01 18:25:30 -0300

    Part 5 started, added userAuth

---
+++ part5/bloglist-frontend-main/src/App.jsx
@@ -0,0 +1,84 @@
+import { useState, useEffect } from "react";
+import Blog from "./components/Blog";
+import blogService from "./services/blogs";
+import loginService from "./services/login";
+
+const App = () => {
+  const [blogs, setBlogs] = useState([]);
+  const [user, setUser] = useState(null);
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [newBlog, setNewBlog] = useState("");
+
+  useEffect(() => {
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      });
+      window.localStorage.setItem("loggedUser", JSON.stringify(user));
+      blogService.setToken(user.token);
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (exception) {
+      setErrorMessage("Wrong credentials");
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
+  };
+
+  const addBlog = (event) => {
+    event.preventDefault();
+    const blogObject = {};
+
+    blogService.create(blogObject).then((response) => {
+      setBlogs(blogs.concat(response));
+      setNewBlog("");
+    });
+  };
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  );
+
+  const blogsRenderer = () => (
+    <>
+      <h2>blogs</h2>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
+    </>
+  );
+
+  return <div>{user === null ? loginForm() : blogsRenderer()}</div>;
+};
+
+export default App;
