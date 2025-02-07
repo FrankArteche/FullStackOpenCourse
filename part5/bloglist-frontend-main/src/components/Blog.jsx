3ecfe20fb0337f530afc91eee558cc6b80ecf14e
Frank <franco.arteche.2b@gmail.com>
2025-02-07 00:17:33 -0300

    Finished exercise 5.7 kindof

--- part5/bloglist-frontend-main/src/components/Blog.jsx
+++ part5/bloglist-frontend-main/src/components/Blog.jsx
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [viewDetails, setViewDetails] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <div style={{ display: "flex" }}>
+          {viewDetails && (
+            <div style={{ display: "grid" }}>
+              <div>{blog.url}</div>
+              <div>likes {blog.likes} <button>like</button></div>
+            </div>
+          )}
+        </div>
+      </div>
+      <div>
+        <button onClick={() => setViewDetails(!viewDetails)}>
+          {viewDetails ? "hide" : "view"}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
