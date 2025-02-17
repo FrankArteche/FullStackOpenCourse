6d65b1b5781ed9521c458fac0b9dd3daaa042c27
Frank <franco.arteche.2b@gmail.com>
2025-02-17 00:49:38 -0300

    Exercise 5.13 done
    - Added testing to Blog

---
+++ part5/bloglist-frontend-main/src/components/Blog.test.jsx
@@ -0,0 +1,38 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Blog from "./Blog";
+
+test("renders blogs title and author but not URL or likes", async () => {
+  const blog = {
+    title: "Testing is kinda a pain in the a**",
+    author: "A man named Jerry",
+    url: "mockupurl.com",
+    likes: 1,
+  };
+
+  const mockHandler = vi.fn();
+
+  render(
+    <Blog
+      blog={blog}
+      onLike={mockHandler}
+      onError={mockHandler}
+      canDelete={false}
+    />
+  );
+
+  const title = screen.getByText("Testing is kinda a pain in the a**", {
+    exact: false,
+  });
+  const author = screen.getByText("A man named Jerry", { exact: false });
+
+  const url = screen.queryByText("mockupurl.com");
+
+  const likes = screen.queryByText(1);
+
+  expect(title).toBeDefined();
+  expect(author).toBeDefined();
+
+  expect(url).toBeNull();
+  expect(likes).toBeNull();
+});
