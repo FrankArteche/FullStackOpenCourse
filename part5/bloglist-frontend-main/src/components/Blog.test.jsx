88bc4dfc2f8cc0af026daaaa0bd39700c31b1ea0
Frank <franco.arteche.2b@gmail.com>
2025-02-17 00:58:41 -0300

    Exercise 5.14 done
    - Added another test for clicking blog details

--- part5/bloglist-frontend-main/src/components/Blog.test.jsx
+++ part5/bloglist-frontend-main/src/components/Blog.test.jsx
@@ -36,3 +36,37 @@ test("renders blogs title and author but not URL or likes", async () => {
   expect(url).toBeNull();
   expect(likes).toBeNull();
 });
+
+test("URL and likes are not shown until button has been clicked", async () => {
+  const blog = {
+    title: "Testing is kinda a pain in the a**",
+    author: "A man named Jerry",
+    url: "mockupurl.com",
+    likes: 1,
+    user: {
+        name: "Frank"
+    }
+  };
+
+  const mockHandler = vi.fn();
+
+  render(
+    <Blog
+      blog={blog}
+      onLike={mockHandler}
+      onError={mockHandler}
+      canDelete={false}
+    />
+  );
+
+  const user = userEvent.setup();
+  const button = screen.getByText("view");
+  await user.click(button);
+
+  const url = screen.queryByText("mockupurl.com");
+
+  const likes = screen.queryByText(1);
+
+  expect(likes).toBeDefined();
+  expect(url).toBeDefined();
+});
